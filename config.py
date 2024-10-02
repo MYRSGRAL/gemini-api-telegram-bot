5c14a8526ce24772abf3cfba35111086da018f25
Murzaev Daniil <112949401+MYRSGRAL@users.noreply.github.com>
2024-10-02 17:24:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_TOKEN = 'Telegram API token' # https://t.me/BotHelp_Fatherbot
+API_TOKEN = "Telegram API token"  # https://t.me/BotHelp_Fatherbot
 GOOGLE_API_KEY_list = [
-    "Frist api key", # https://aistudio.google.com/app/apikey
-    "Second api key ", # https://aistudio.google.com/app/apikey
+    "Frist api key",  # https://aistudio.google.com/app/apikey
+    "Second api key ",  # https://aistudio.google.com/app/apikey
 ]

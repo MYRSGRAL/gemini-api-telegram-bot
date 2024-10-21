f6aa68625dca5fabf4474ce6e9bd33888987070b
MYRSGRAL <murzaeff.daniil@yandex.ru>
2024-10-21 17:57:21 +0300

    add ChatAction typing, message about working

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.types import Message, callback_query
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.utils.chat_action import ChatActionSender
+from aiogram.enums import ChatAction
 
 from config import (API_TOKEN, GOOGLE_API_KEY_list)
 from Handlers import (load_settings, get_user_model, set_user_model, load_conversation_history, save_conversation_history, delete_folder)
@@ -67,124 +69,128 @@ async def clear_history(message: types.Message):
 
 @dp.message()
 async def handle_message(message: Message):
-    for GOOGLE_API in GOOGLE_API_KEY_list:
+    msg = await message.answer("Пожалуйста, подождите, идет обработка запроса")
+    async with ChatActionSender(action=ChatAction.TYPING, chat_id=message.chat.id, bot=bot):
+        await asyncio.sleep(0.01)
         try:
-            genai.configure(api_key=GOOGLE_API)
-            history_json = f'{message.chat.id}.json'
-            if not os.path.exists(history_json):
-                open(history_json, 'w').close()
-            conversation_history = load_conversation_history(history_json)
+            for GOOGLE_API in GOOGLE_API_KEY_list:
+                try:
+                    genai.configure(api_key=GOOGLE_API)
+                    history_json = f'{message.chat.id}.json'
+                    if not os.path.exists(history_json):
+                        open(history_json, 'w').close()
+                    conversation_history = load_conversation_history(history_json)
 
-            if message.content_type == ContentType.TEXT:
-                text = message.text
-                if len(text) > 4000:
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Максимальная длина сообщения 4000 символов", reply_markup=main_keyboard)
-                    break
-                conversation_history.append({"role": "user", "parts": [{"text": text}]})
-            elif message.content_type == ContentType.PHOTO:
-                text = message.caption
-                if text is None:
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Пожалуйста, введите подпись к изображению",
-                                         reply_markup=main_keyboard)
-                    break
-                if len(text) > 1000:
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Максимальная длина подписи фото 1000 символов",
-                                         reply_markup=main_keyboard)
-                    break
-                photo = message.photo[-1]
-                file_info = await bot.get_file(photo.file_id)
-                file_path = file_info.file_path
-                telegram_id = message.from_user.id
-                media_dir = f'media/{telegram_id}'
-                if not os.path.exists(media_dir):
-                    os.makedirs(media_dir)
-                file_name = f'{media_dir}/{photo.file_id}.jpg'
-                await bot.download_file(file_path, file_name)
-                image = PIL.Image.open(file_name)
-                if image is None:
-                    raise ValueError("Failed to open image")
-                conversation_history.append({"role": "user", "parts": [{"text": text}]})
-            elif message.content_type == ContentType.DOCUMENT:
-                text = message.caption
-                if text is None:
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Пожалуйста, введите подпись к документу",
-                                         reply_markup=main_keyboard)
-                    break
-                if len(text) > 1000:
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Максимальная длина подписи файла 1000 символов",
-                                         reply_markup=main_keyboard)
-                    break
-                document = message.document
-                file_info = await bot.get_file(document.file_id)
-                file_path = file_info.file_path
-                telegram_id = message.from_user.id
-                media_dir = f'media/{telegram_id}'
-                if not os.path.exists(media_dir):
-                    os.makedirs(media_dir)
-                file_name = f'{media_dir}/{document.file_name}'
-                await bot.download_file(file_path, file_name)
-                if not message.document.mime_type == "application/pdf":
-                    upload_file_s = genai.upload_file(file_name)
-                    conversation_history.append({"role": "user", "parts": [{"text": text}]})
-                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                    ])
-                    await message.answer("Поддерживается только pdf", reply_markup=main_keyboard)
-                    break
+                    if message.content_type == ContentType.TEXT:
+                        text = message.text
+                        if len(text) > 4000:
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Максимальная длина сообщения 4000 символов", reply_markup=main_keyboard)
+                            break
+                        conversation_history.append({"role": "user", "parts": [{"text": text}]})
+                    elif message.content_type == ContentType.PHOTO:
+                        text = message.caption
+                        if text is None:
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Пожалуйста, введите подпись к изображению",
+                                                 reply_markup=main_keyboard)
+                            break
+                        if len(text) > 1000:
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Максимальная длина подписи фото 1000 символов",
+                                                 reply_markup=main_keyboard)
+                            break
+                        photo = message.photo[-1]
+                        file_info = await bot.get_file(photo.file_id)
+                        file_path = file_info.file_path
+                        telegram_id = message.from_user.id
+                        media_dir = f'media/{telegram_id}'
+                        if not os.path.exists(media_dir):
+                            os.makedirs(media_dir)
+                        file_name = f'{media_dir}/{photo.file_id}.jpg'
+                        await bot.download_file(file_path, file_name)
+                        image = PIL.Image.open(file_name)
+                        if image is None:
+                            raise ValueError("Failed to open image")
+                        conversation_history.append({"role": "user", "parts": [{"text": text}]})
+                    elif message.content_type == ContentType.DOCUMENT:
+                        text = message.caption
+                        if text is None:
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Пожалуйста, введите подпись к документу",
+                                                 reply_markup=main_keyboard)
+                            break
+                        if len(text) > 1000:
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Максимальная длина подписи файла 1000 символов",
+                                                 reply_markup=main_keyboard)
+                            break
+                        document = message.document
+                        file_info = await bot.get_file(document.file_id)
+                        file_path = file_info.file_path
+                        telegram_id = message.from_user.id
+                        media_dir = f'media/{telegram_id}'
+                        if not os.path.exists(media_dir):
+                            os.makedirs(media_dir)
+                        file_name = f'{media_dir}/{document.file_name}'
+                        await bot.download_file(file_path, file_name)
+                        if not message.document.mime_type == "application/pdf":
+                            upload_file_s = genai.upload_file(file_name)
+                            conversation_history.append({"role": "user", "parts": [{"text": text}]})
+                            main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                                [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                                [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                            ])
+                            await message.answer("Поддерживается только pdf", reply_markup=main_keyboard)
+                            break
 
-            user_id = message.from_user.id
-            model_name = get_user_model(settings, user_id)
-            model = genai.GenerativeModel(model_name)
+                    user_id = message.from_user.id
+                    model_name = get_user_model(settings, user_id)
+                    model = genai.GenerativeModel(model_name)
 
-            if message.content_type == ContentType.TEXT:
-                chat_session = model.start_chat(history=conversation_history)
-                response = chat_session.send_message(text)
-            elif message.content_type == ContentType.PHOTO:
-                response = model.generate_content([text, image])
-            elif message.content_type == ContentType.DOCUMENT:
-                response = model.generate_content([text, upload_file_s])
+                    if message.content_type == ContentType.TEXT:
+                        chat_session = model.start_chat(history=conversation_history)
+                        response = chat_session.send_message(text)
+                    elif message.content_type == ContentType.PHOTO:
+                        response = model.generate_content([text, image])
+                    elif message.content_type == ContentType.DOCUMENT:
+                        response = model.generate_content([text, upload_file_s])
 
-            conversation_history.append({"role": "model", "parts": [{"text": response.text}]})
-            save_conversation_history(conversation_history, history_json)
+                    conversation_history.append({"role": "model", "parts": [{"text": response.text}]})
+                    save_conversation_history(conversation_history, history_json)
 
+                    main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                        [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
+                        [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
+                    ])
+                    await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id,
+                                                text=response.text, reply_markup=main_keyboard, parse_mode=ParseMode.MARKDOWN)
+                    break
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
             main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
                 [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
                 [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
             ])
-            await message.answer(response.text, reply_markup=main_keyboard, parse_mode=ParseMode.MARKDOWN)
-            break
-
-        except Exception as e:
-            if str(e) == "Telegram server says - Bad Request: message is too long":
-                main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                    [InlineKeyboardButton(text="Очистить историю", callback_data="Del_history")],
-                    [InlineKeyboardButton(text="Сменить модель", callback_data="Change_model")]
-                ])
-                await message.answer(f"Error: {e} \n\nПопробуйте очистить историю или уменьшить сообщение", reply_markup=main_keyboard)
-            else:
-                print(e)
-
+            await bot.edit_message_text(chat_id=message.chat.id, message_id=msg.message_id,
+                                        text="Error: Произошла ошибка", reply_markup=main_keyboard)
 
 async def main():
     await dp.start_polling(bot)

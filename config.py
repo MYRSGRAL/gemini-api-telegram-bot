aaf0211f9d457c834930aa6fdaf06296b040e305
MYRSGRAL <murzaeff.daniil@yandex.ru>
2024-10-24 12:03:01 +0300

    change json to sqlite3, add settings

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_TOKEN = "8020278235:AAHmEHbxWAcvUVt-x7gMMtHVHALDPgOWbOo"  # https://t.me/BotHelp_Fatherbot
+API_TOKEN = "Telegram API token"  # https://t.me/BotHelp_Fatherbot
 GOOGLE_API_KEY_list = [
-    "AIzaSyBgK_7OhkxCXK1oSpY2AKFe8r5wvivUeis",  # https://aistudio.google.com/app/apikey
-    "AIzaSyBgK_7OhkxCXK1oSpY2AKFe8r5wvivUeis",  # https://aistudio.google.com/app/apikey
+    "Frist api key",  # https://aistudio.google.com/app/apikey
+    "Second api key ",  # https://aistudio.google.com/app/apikey
 ]
 DEFAULT_MODEL = 'gemini-1.5-flash'  # Default model to use
 history_json = None  # Stable variable that cannot be changed

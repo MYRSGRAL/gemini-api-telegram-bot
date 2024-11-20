6733ab166350ca8974fb38840e1b9b124c0441b2
MYRSGRAL <murzaeff.daniil@yandex.ru>
2024-11-20 17:24:09 +0300

    Fix typo in google API key list
    Corrected the spelling of "First" in the GOOGLE_API_KEY_list. Ensures consistency and accuracy in configuration naming.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_TOKEN = "Telegram API token"  # https://t.me/BotHelp_Fatherbot
 GOOGLE_API_KEY_list = [
-    "Frist api key",  # https://aistudio.google.com/app/apikey
+    "First api key",  # https://aistudio.google.com/app/apikey
     "Second api key ",  # https://aistudio.google.com/app/apikey
 ]
 DEFAULT_MODEL = 'gemini-1.5-flash'  # Default model to use
